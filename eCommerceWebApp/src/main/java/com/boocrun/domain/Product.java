e7fd755724d5ad2f54bc4d90d8c558ff8fc7195b
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2022-08-16 21:33:50 -0400

    apdate files

---
+++ eCommerceWebApp/src/main/java/com/boocrun/domain/Product.java
@@ -0,0 +1,102 @@
+package com.boocrun.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+
+//import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Product {
+	private Long id;
+	private Byte[] image;
+	private Double price;
+	private String description;
+	private String shortDescription;
+	private User user;
+	private Set<Review> reviews = new HashSet<>();
+	private Set<Cart> carts = new HashSet<>();
+
+	@ManyToOne
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "product" )
+	public Set<Review> getReviews() {
+		return reviews;
+	}
+
+	public void setReviews(Set<Review> reviews) {
+		this.reviews = reviews;
+	}
+
+	@ManyToMany(cascade = CascadeType.ALL, mappedBy = "products")
+	public Set<Cart> getCarts() {
+		return carts;
+	}
+
+	public void setCarts(Set<Cart> carts) {
+		this.carts = carts;
+	}
+
+	@Id
+	@GeneratedValue
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Byte[] getImage() {
+		return image;
+	}
+
+	public void setImage(Byte[] image) {
+		this.image = image;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+	
+
+	
+	
+	
+
+}
---
+++ eCommerceWebApp/src/main/java/com/boocrun/domain/Review.java
@@ -0,0 +1,73 @@
+package com.boocrun.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Review {
+	private Long id;
+	private String text;
+	private Integer rating;
+	private Date date;
+	private User uaser;
+	private Product product;
+	
+	
+	
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Integer getRating() {
+		return rating;
+	}
+
+	public void setRating(Integer rating) {
+		this.rating = rating;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	@Id
+	@GeneratedValue
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	@ManyToOne
+	public User getUaser() {
+		return uaser;
+	}
+
+	public void setUaser(User uaser) {
+		this.uaser = uaser;
+	}
+
+	@ManyToOne
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+}
