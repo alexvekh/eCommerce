c7043199df3fe3332d54219906bfba209a995e8c
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2022-12-01 00:24:06 -0500

    I don't remember what is here

--- eCommerceWebApp/src/main/java/com/boocrun/security/SecurityConfig.java
+++ eCommerceWebApp/src/main/java/com/boocrun/security/SecurityConfig.java
@@ -4,10 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
 	private UserDetailsService userDetailsService;
@@ -19,14 +20,20 @@ public class SecurityConfig {
 	}
 	
 
-	public void configureGlobal (HttpSecurity http) throws Exception 
+	@Override
+ 	protected void configure(HttpSecurity http) throws Exception
 	{
-		http.authorizeRequests()
+		http
+			.csrf().disable()
+			.authorizeRequests()
+			.antMatchers("/**").hasAnyRole("DASHBOARD")
+			//.antMatchers("/**").hasAnyAuthority("USER_DASHBOARD")
 			.anyRequest().authenticated()
 			.and()
 		.formLogin()
 			.loginPage("/login")
-			.defaultSuccessUrl("{noop}/dashboard");
+			.defaultSuccessUrl("/dashboard")
+			.permitAll();
 	
 	}
 }
--- eCommerceWebApp/src/main/java/com/boocrun/web/LoginController.java
+++ eCommerceWebApp/src/main/java/com/boocrun/web/LoginController.java
@@ -6,13 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class DashboardController {
-	@RequestMapping(value="/dashboard", method=RequestMethod.GET)
-	public String dasboardGet (ModelMap model)
+public class LoginController {
+	
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public String LoginGet (ModelMap model)
 	{
-		return "/dashboard";
+		return "login";
 	}
-	
-	
-	
 }
